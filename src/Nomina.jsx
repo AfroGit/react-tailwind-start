440690a79c728be5a1ebfd029a35ec333679a96b
AfroGit <17697323-AfroGit@users.noreply.replit.com>
2023-07-06 17:07:19 +0000

    This template has enabled me to get started with Tailwind css. To crank it up required updating versions of Tailwind,vite and node.
    Nomina was created as a test bed for the basic function of making sure everything was working.

--- src/Nomina.jsx
+++ src/Nomina.jsx
@@ -1,7 +1,7 @@
 export default function Nomina(){
   return(
     <>
-      <button className="bg-yellow-300 text-white rounded-lg px-1 mx-0.5 items-center ">Bonya</button>
+      <button className="bg-yellow-300 text-white rounded-lg px-1 mx-0.5 items-center w-28 ">Bonya</button>
     </>
   )
 };
